fdbc38364ac2359072b306d8a8f105eee92cd97b
ezekielKouassi-dev <ezekielkouassi836@yahoo.com>
2023-03-09 07:18:12 +0000

    add comment to my user payload

--- src/main/java/di/learning/clean/ma/ci/model/UserPayload.java
+++ src/main/java/di/learning/clean/ma/ci/model/UserPayload.java
@@ -1,5 +1,11 @@
 package di.learning.clean.ma.ci.model;
 
+/**
+ *
+ * this POJO is a recipient, I use it for retrieve user registration data
+ * before insertion in database
+ */
+
 public class UserPayload {
     String lastName;
     String firstName;
