1be352a79cb3c489589ec408a943f9bded046983
ezekielKouassi-dev <ezekielkouassi836@yahoo.com>
2023-03-20 19:35:41 +0000

    add stats methods

--- src/main/java/di/learning/clean/ma/ci/controller/StatisticController.java
+++ src/main/java/di/learning/clean/ma/ci/controller/StatisticController.java
@@ -1,7 +1,30 @@
 package di.learning.clean.ma.ci.controller;
 
+import di.learning.clean.ma.ci.entity.AssignmentUser;
+import di.learning.clean.ma.ci.service.assignmentUser_.AssignmentUserService;
+import di.learning.clean.ma.ci.service.statistic_.StatisticService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api/v1/statistics")
+@CrossOrigin(origins = "http://localhost:4200/")
 public class StatisticController {
+
+    @Autowired
+    private StatisticService statisticService;
+
+    @Autowired
+    private AssignmentUserService assignmentUserService;
+
+
+    /*
+    * 1- found list of all assignment that adherent has been completed by processingCompany and count it
+    * then put it into statistics table
+    * 2- request in userRepository for get all user assignment who leave any assignment
+    * */
+
 }
--- src/main/java/di/learning/clean/ma/ci/repository/AssignmentUserRepository.java
+++ src/main/java/di/learning/clean/ma/ci/repository/AssignmentUserRepository.java
@@ -14,4 +14,15 @@ public interface AssignmentUserRepository extends JpaRepository<AssignmentUser, 
     public List<AssignmentUser> findAllByAdherentAndState(Adherent adherent, String state);
     public List<AssignmentUser> findAssignmentUserByAdherent(Adherent adherent);
     public List<AssignmentUser> findAllByAssignmentAndState(Assignment assignment, String state);
+    /*
+    * method for count all assignment that has been completed, leave or in progress for a specific adherent
+    * */
+    public double countAllByAdherent_UserIdAndState(Long adherentId, String state);
+
+    /*
+    * method for count all user which completed, leave or progress assignment for a specific processing company
+    * */
+    public double countAllByAssignment_ProcessingCompany_UserIdAndState(Long processingCompanyId, String state);
+
+    public double countAllByAssignment_PointOfDrop_Locality_NameAndState();
 }
